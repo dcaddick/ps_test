1e9e85316c2bf538ac26ae801b33869f4ebbbe39
David Caddick <davidcaddick@gmail.com>
2017-05-02 16:40:15 +1000

    Delete test v1.4_obs.ps1

--- test v1.4_obs.ps1
+++
@@ -1,2 +0,0 @@
-﻿${1} = New-Object -ComObject Wscript.Shell
-${1}.Popup($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('VABoAGkAcwAgAGkAcwAgAGEAIAB0AGUAcwB0ACAAZQB4AGEAbQBwAGwAZQAgAG8AZgAgAHcAaABhAHQAIABjAG8AdQBsAGQAIABoAGEAdgBlACAAYgBlAGUAbgAgAHcAZQBhAHAAbwBuAGkAegBlAGQAIABtAGEAbAB3AGEAcgBlAA=='))),0,$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('RABvAG4AZQA='))),0x1)
